4e4c3e41fff22f532a73023fdc6879664b53ef05
Fear&$H@king <Shakezula20@gmail.com>
2024-10-18 00:29:20 -0400

    Update ShakePSModule.psm1

--- ShakePSModule.psm1
+++ ShakePSModule.psm1
@@ -447,7 +447,7 @@ function flushdns {
 ## Shows DNS Servers ##
 function showdns {
     Get-DnsClientServerAddress | 
-    Where-Object { $_.InterAlias -eq "Wi-Fi" } | 
+    Where-Object { $_.InterfaceAlias -eq "Wi-Fi" } | 
     Select-Object @{
         Name = 'AddressFamily'; 
         Expression = { "IPv$($_.AddressFamily -eq 23 ? 6 : 4)" }},@{Name = 'ServerAddresses'; 
