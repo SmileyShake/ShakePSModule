80700fd63266118e13330cf92d86883db183f9f9
Fear&$H@king <Shakezula20@gmail.com>
2024-11-01 17:30:06 -0400

    Update ShakePSModule.psm1

--- ShakePSModule.psm1
+++ ShakePSModule.psm1
@@ -381,7 +381,7 @@ function winpick {
 
     # Prepare a fixed-width format for fzf
     $formattedAppList = $AppObject | ForEach-Object {
-        '{0,-70} {1,-15} {2}' -f $_.Name, $_.Version, $_.Id
+        '{0,-65} {1,-20} {2}' -f $_.Name, $_.Version, $_.Id
     }
 
     # Select an app via fzf
