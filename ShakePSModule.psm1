ed73cd080f6619ff9bf1f2d1286fa6587a11598c
Fear&$H@king <Shakezula20@gmail.com>
2024-11-01 16:27:29 -0400

    Update ShakePSModule.psm1

--- ShakePSModule.psm1
+++ ShakePSModule.psm1
@@ -380,7 +380,7 @@ function winpick {
     $selectApp = $WingetCommand | Select-Object Name , Version , Id
     $selectId = $selectApp | fzf --prompt=" Select a package: "
     if ($selectId) {
-        $selectId =  $selectId -replace '┬«', '®' -replace 'ΓÇô', '-' -replace 'ΓÇª', '  '
+        $selectId =  $selectId -replace '┬«', '®' -replace 'ΓÇô', '-' -replace 'ΓÇª', ' '
         $selectAppId = $selectId -split '  ' | Select-Object -Last 1
         $selectAppName = $selectId -split '  ' | Select-Object -First 1
         $selectAppId = $selectAppId.TrimStart()        
